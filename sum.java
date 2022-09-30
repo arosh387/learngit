1b324336259812ab96354b87276d8a853cd2ce35
aroshmishra <arosh.mishra@nagarro.com>
2022-09-30 12:52:29 +0530

    added sg in sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
 public class sum{
     public static void main(String[] args){
-        
+
     int a = 5;
     int b = 6;
 
-    System.out.println(a+b); 
+    System.out.println("this is the sum" +(a+b)); 
 }
 }
